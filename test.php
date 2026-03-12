84c25f28b558437be4ecb93fdccf2c80058ce69a
Noorullah <nooriallah18@gmail.com>
2026-03-12 12:18:29 +0430

    New project setup

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "livewire_maktabkhoone";
+
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}
+
+$sql = "SELECT * FROM posts";
+$result = mysqli_query($conn, $sql);
+
+
